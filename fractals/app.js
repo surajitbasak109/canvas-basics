ea23accb41abf1502be7ff1527b4daf4b02dd0b5
Surajit Basak <surajitb@packt.com>
2023-07-30 12:25:39 +0530

    UI controls: randomize button added

--- fractals/app.js
+++ fractals/app.js
@@ -5,7 +5,6 @@ window.addEventListener('load', () => {
   canvas.height = window.innerHeight;
   //   canvas settings
   ctx.fillStyle = 'green';
-  ctx.lineWidth = 10;
   ctx.lineCap = 'round';
 
   //   shadow effect
@@ -15,13 +14,17 @@ window.addEventListener('load', () => {
   ctx.shadowBlur = 10;
 
   // effect settings
-  let size = canvas.width < canvas.height ? canvas.width * 0.3 : canvas.height * 0.3;
+  const maxLevel = 4;
+  const branches = 2;
+  const size =
+    canvas.width < canvas.height ? canvas.width * 0.3 : canvas.height * 0.3;
   let sides = 5;
-  let maxLevel = 3;
   let scale = 0.5;
   let spread = 0.5;
-  let branches = 2;
   let color = `hsl(${Math.random() * 360},100%,50%)`;
+  let lineWidth = Math.floor(Math.random() * 20 + 10);
+  //   controls
+  const randomizeButton = document.getElementById('randomizeButton');
 
   function drawBranch(level) {
     if (level > maxLevel) return;
@@ -34,22 +37,26 @@ window.addEventListener('load', () => {
     for (let i = 0; i < branches; i++) {
       ctx.save();
       ctx.translate(size - (size / branches) * i, 0);
-      ctx.rotate(spread);
       ctx.scale(scale, scale);
+
+      ctx.save();
+      ctx.rotate(spread);
       drawBranch(level + 1);
       ctx.restore();
 
       ctx.save();
-      ctx.translate(size - (size / branches) * i, 0);
-      ctx.rotate(-spread);
       ctx.scale(scale, scale);
       drawBranch(level + 1);
+      ctx.restore();
+
       ctx.restore();
     }
   }
 
   function drawFractal() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.save();
+    ctx.lineWidth = lineWidth;
     ctx.strokeStyle = color;
     ctx.translate(canvas.width / 2, canvas.height / 2);
 
@@ -60,5 +67,17 @@ window.addEventListener('load', () => {
     ctx.restore();
   }
 
+  function randomizeFractal() {
+    sides = Math.floor(Math.random() * 7 + 2);
+    scale = Math.random() * 0.2 + 0.4;
+    spread = Math.random() * 2.9 + 0.1;
+    color = `hsl(${Math.random() * 360},100%,50%)`;
+    lineWidth = Math.floor(Math.random() * 20 + 10);
+  }
+
   drawFractal();
+  randomizeButton.addEventListener('click', () => {
+    randomizeFractal();
+    drawFractal();
+  });
 });
