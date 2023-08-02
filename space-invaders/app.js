823eca789cf5ff24587f7ce47331f6ac618a8db5
Surajit Basak <surajitb@packt.com>
2023-08-03 05:14:02 +0530

    Adding keyboard controls

--- space-invaders/app.js
+++ space-invaders/app.js
@@ -3,7 +3,22 @@ class Game {
     this.canvas = canvas;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
+    this.keys = [];
     this.player = new Player(this);
+
+    // event listeners
+    window.addEventListener('keydown', (event) => {
+      if (this.keys.indexOf(event.key) === -1) {
+        this.keys.push(event.key);
+      }
+    })
+
+    window.addEventListener('keyup', (event) => {
+      const index = this.keys.indexOf(event.key);
+      if (index > -1) {
+        this.keys.splice(index, 1);
+      }
+    })
   }
 
   render(context) {
@@ -19,7 +34,7 @@ class Player {
     this.height = 100;
     this.x = this.game.width * 0.5 - this.width * 0.5;
     this.y = this.game.height - this.height;
-    this.speed = 5;
+    this.speed = 20;
   }
 
   draw(context) {
@@ -27,7 +42,11 @@ class Player {
   }
 
   update() {
-    this.x += this.speed;
+    if (this.game.keys.indexOf('ArrowLeft') > -1 && this.x > 0) {
+      this.x -= this.speed;
+    } else if (this.game.keys.indexOf('ArrowRight') > -1 && this.x < this.game.width - this.width) {
+      this.x += this.speed;
+    }
   }
 }
 
