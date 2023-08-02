7c9a19d6413dc137635a9790657d5ca451432fc0
Surajit Basak <surajitb@packt.com>
2023-08-03 04:51:24 +0530

    project setup for space invaders

---
+++ space-invaders/app.js
@@ -0,0 +1,21 @@
+class Game {
+}
+
+class Player {
+
+}
+
+class Projectile {
+
+}
+
+class Enemy {
+
+}
+
+window.addEventListener('load', () => {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 600;
+  canvas.height = 800;
+})
