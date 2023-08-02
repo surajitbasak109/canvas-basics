39779b2fc22f1b52d423ac0758cdcc189030fd49
Surajit Basak <surajitb@packt.com>
2023-08-03 04:59:57 +0530

    Adding game and player objects

--- space-invaders/app.js
+++ space-invaders/app.js
@@ -1,8 +1,34 @@
 class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.player = new Player(this);
+  }
+
+  render(context) {
+    this.player.draw(context);
+    this.player.update();
+  }
 }
 
 class Player {
+  constructor (game) {
+    this.game = game;
+    this.width = 100;
+    this.height = 100;
+    this.x = this.game.width * 0.5 - this.width * 0.5;
+    this.y = this.game.height - this.height;
+    this.speed = 5;
+  }
 
+  draw(context) {
+    context.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  update() {
+    this.x += this.speed;
+  }
 }
 
 class Projectile {
@@ -18,4 +44,14 @@ window.addEventListener('load', () => {
   const ctx = canvas.getContext('2d');
   canvas.width = 600;
   canvas.height = 800;
+
+  const game = new Game(canvas);
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.render(ctx);
+    requestAnimationFrame(animate);
+  }
+
+  animate();
 })
